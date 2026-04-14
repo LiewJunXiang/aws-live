957ba8b3dfeeb5b9ff43ecfcb58d61523aa4b4ea
LiewJunXiang <liewjx-wm23@student.tarc.edu.my>
2026-04-15 07:59:18 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cin4yt1uub7o.us-east-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "BMIT3273"
+customdb = "employee"
+custombucket = "liewjunxiang-employee"
+customregion = "us-east-1a"
+
